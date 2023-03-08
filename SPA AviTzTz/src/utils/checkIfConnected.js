066e9823ec778e24cd8bf75a9b36c28a885db103
AviTzTz <55462990+AviTzTz@users.noreply.github.com>
2023-03-08 03:28:51 +0200

    Delete SPA AviTzTz directory

--- SPA AviTzTz/src/utils/checkIfConnected.js
+++
@@ -1,9 +0,0 @@
-const checkIfConnected = () => {
-    let token = JSON.parse(localStorage.getItem("token"));
-    if (!token) {
-      return false;
-    }
-    return !!token; //convert to boolean
-  };
-  
-export default checkIfConnected;
--- SPA AviTzTz/src/validation/validateName.js
+++
@@ -1,8 +0,0 @@
-import validate from "./validate.js";
-
-const validateName = (value) => {
-  const regex = new RegExp("^[A-Z][a-z0-9-\\s]{0,}$", "g");
-  return validate("nameValidation",regex, value, 2, 30).map((err) => `Name ${err}`);
-};
-
-export default validateName;
